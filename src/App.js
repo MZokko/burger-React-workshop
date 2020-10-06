684b821362f5989c00a2f40de48aa8a41a676bd6
alexandre <akekiwy@gmail.com>
2020-10-06 16:54:01 +1100

    settingUpRoute NavigationCheckout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
+import {Route, Switch} from 'react-router-dom';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
@@ -11,8 +12,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout/> 
+          <Switch>
+            <Route path="/checkout" component={Checkout}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          </Switch>
         </Layout>
       </div>
     );
