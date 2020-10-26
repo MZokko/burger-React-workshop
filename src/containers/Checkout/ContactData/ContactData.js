6c2bae7da7e9013f715a0b872898798dc68e03c0
alexandre <akekiwy@gmail.com>
2020-10-26 13:53:32 +1100

    Dynamically Inputs JS Config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -70,29 +70,27 @@ class ContactData extends Component {
   };
 
   render() {
+    const formElementArray = [];
+
+    for (let key in this.state.orderForm) {
+      formElementArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
+
     let form = (
       <form>
-        <Input
-          elementType="" elementConfig="" value=""
-        />
-        <Input
-          inputtype='input'
-          type='email'
-          name='email'
-          placeholder='email@email.email'
-        />
-        <Input
-          inputtype='input'
-          type='text'
-          name='street'
-          placeholder='your street'
-        />
-        <Input
-          inputtype='input'
-          type='text'
-          name='postal'
-          placeholder='your postal code'
-        />
+        {formElementArray.map((formElement) => {
+          return (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+            />
+          );
+        })}
         <Button btnType='Success' clicked={this.orderHandler}>
           ORDER
         </Button>
