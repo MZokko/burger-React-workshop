4e96c318bcb8f30729a54c943cb4c40601601a1e
alexandre <akekiwy@gmail.com>
2020-11-03 12:51:16 +1100

    manage price thru redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,13 @@ const initialState = {
   totalPrice: 4,
 };
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
@@ -16,18 +23,22 @@ const reducer = (state = initialState, action) => {
           //Dynamically Override
           [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
         },
+        //Recalculate the price
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
       };
 
     case actionTypes.REMOVE_INGREDIENT:
-        return {
-            //find what ingredient , add +1
-            ...state,
-            ingredients: {
-              ...state.ingredients,
-              //Dynamically Override
-              [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
-            },
-          };
+      return {
+        //find what ingredient , add +1
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          //Dynamically Override
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+        },
+        //Recalculate the price
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+      };
 
     default:
       return state;
