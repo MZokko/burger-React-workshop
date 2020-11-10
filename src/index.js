349ccddd576213eff178318c21796f878b32b5d5
alexandre <akekiwy@gmail.com>
2020-11-10 17:03:03 +1100

    redux thunk set

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(burgerBuilderReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); //create the store and pass the reducer+ react dev tool
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  burgerBuilderReducer,
+  composeEnhancers(applyMiddleware(thunk))
+); //create the store and pass the reducer+ react dev tool
 
 //provider must include app, wrap all
 const app = (
-  <Provider store = {store}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
