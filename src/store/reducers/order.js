b5f78065d51ac1492d7c6209910548778c7df305
alexandre <akekiwy@gmail.com>
2020-12-01 16:40:03 +1100

    order reducer set up

---
+++ src/store/reducers/order.js
@@ -0,0 +1,34 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  orders: [],
+  loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.PURCHASE_BURGER_SUCCESS:
+      // merge order data and order id in obj
+      const newOrder = {
+        ...action.orderData,
+        id: action.orderId,
+      };
+      return {
+        //store the order in order array && set loading to false
+        ...state,
+        loading: false,
+        orders: state.orders.concat(newOrder), // concat return a new array <= inmutable
+      };
+
+    case actionTypes.PURCHASE_BURGER_FAIL:
+      return {
+        ...state,
+        loading: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
