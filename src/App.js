f27a5b60ecd1be98eaff8b57cd9915c80a040baf
alexandre <akekiwy@gmail.com>
2021-02-09 13:09:08 +1100

    ui fix

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
+          <Route path='/auth' component={Auth} />
           <Route path='/' exact component={BurgerBuilder} />
           <Route path='/checkout' component={Checkout} />
           <Route path='/orders' component={Orders} />
